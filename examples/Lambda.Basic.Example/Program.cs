dcd1afaf2a711c6d47f9892cdaf1f2e9bff18d30
Andrew Craven <andrew@epworth-consulting.co.uk>
2023-11-16 01:44:10 +0000

    Update NuGet packages in examples (#139)

--- examples/Lambda.Basic.Example/Program.cs
+++ examples/Lambda.Basic.Example/Program.cs
@@ -1,29 +1,11 @@
-﻿using System.Threading.Tasks;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using Amazon.Lambda.Serialization.SystemTextJson;
+using Lambda.Basic.Example;
 using Stackage.Aws.Lambda;
 
-namespace Lambda.Basic.Example
-{
-   public static class Program
-   {
-      public static async Task Main()
-      {
-         // TODO: How best to catch initialisation errors and send to the runtime API
+using var consoleLifetime = new ConsoleLifetime();
 
-         var host = LambdaHost.Create(builder =>
-            {
-               builder.UseSerializer<CamelCaseLambdaJsonSerializer>();
-               builder.UseHandler<EchoLambdaHandler>();
-            })
-            .ConfigureLogging(builder =>
-            {
-               builder.AddJsonConsole();
-            })
-            .Build();
-
-         await host.RunAsync();
-      }
-   }
-}
+await new LambdaListenerBuilder()
+   .UseHandler<EchoLambdaHandler>()
+   .UseSerializer<CamelCaseLambdaJsonSerializer>()
+   .Build()
+   .ListenAsync(consoleLifetime.Token);
--- examples/Lambda.Middleware.Example/Program.cs
+++ examples/Lambda.Middleware.Example/Program.cs
@@ -1,32 +1,14 @@
-using System.Threading.Tasks;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using Lambda.Middleware.Example;
 using Lambda.Middleware.Example.Handler;
 using Lambda.Middleware.Example.Model;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Stackage.Aws.Lambda;
 
-namespace Lambda.Middleware.Example
-{
-   public static class Program
-   {
-      public static async Task Main()
-      {
-         // TODO: How best to catch initialisation errors and send to the runtime API
+using var consoleLifetime = new ConsoleLifetime();
 
-         var host = LambdaHost.Create(builder =>
-            {
-               builder.UseStartup<LambdaStartup>();
-               builder.UseSerializer<CamelCaseLambdaJsonSerializer>();
-               builder.UseHandler<ObjectLambdaHandler, InputPoco>();
-            })
-            .ConfigureLogging(builder =>
-            {
-               builder.AddJsonConsole();
-            })
-            .Build();
-
-         await host.RunAsync();
-      }
-   }
-}
+await new LambdaListenerBuilder()
+   .UseHandler<ObjectLambdaHandler, InputPoco>()
+   .UseStartup<LambdaStartup>()
+   .UseSerializer<CamelCaseLambdaJsonSerializer>()
+   .Build()
+   .ListenAsync(consoleLifetime.Token);
