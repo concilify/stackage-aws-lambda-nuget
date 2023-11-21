684808d11605d0596b74a9f9db8fc546d6ffb97c
Andrew Craven <andrew@epworth-consulting.co.uk>
2023-11-21 00:46:01 +0000

    Update examples

--- examples/Lambda.Middleware.Example/Handler/ObjectLambdaHandler.cs
+++ examples/Lambda.Middleware.Example/Handler/ObjectLambdaHandler.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
-using Lambda.Middleware.Example.Integrations;
 using Lambda.Middleware.Example.Model;
 using Stackage.Aws.Lambda.Abstractions;
+using Stackage.Aws.Lambda.Results;
 
 namespace Lambda.Middleware.Example.Handler
 {
@@ -23,12 +23,24 @@ namespace Lambda.Middleware.Example.Handler
             throw new Exception("Throwing exception from ObjectLambdaHandler");
          }
 
-         if (input.Action == "delay")
+         if (input.Action != null && input.Action.StartsWith("delay"))
          {
-            await Task.Delay(5000, _deadlineCancellation.Token);
+            await Task.Delay(ParseDelay(input.Action), _deadlineCancellation.Token);
          }
 
-         return new HttpObjectResult(new OutputPoco {Action = input.Action});
+         return new ObjectResult(new OutputPoco {Action = input.Action});
+      }
+
+      private static int ParseDelay(string action)
+      {
+         var parts = action.Split(":");
+
+         if (parts.Length <= 1 || !int.TryParse(parts[1], out var delay))
+         {
+            return 5000;
+         }
+
+         return delay;
       }
    }
 }
