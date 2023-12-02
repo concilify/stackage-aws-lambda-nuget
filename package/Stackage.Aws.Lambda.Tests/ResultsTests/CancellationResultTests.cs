ecc447273cbbf6a9b8d6361bbd5d46350915bb5f
Andrew Craven <andrew@epworth-consulting.co.uk>
2023-12-02 23:47:38 +0000

    extra tests

---
+++ package/Stackage.Aws.Lambda.Tests/ResultsTests/CancellationResultTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Stackage.Aws.Lambda.Results;
+
+namespace Stackage.Aws.Lambda.Tests.ResultsTests;
+
+public class CancellationResultTests
+{
+   [Test]
+   public void Message_is_accessible()
+   {
+      var testSubject = new CancellationResult("ArbitraryMessage");
+
+      Assert.That(testSubject.Message, Is.EqualTo("ArbitraryMessage"));
+   }
+}
